ea7916d45f364bd30abb9d2545c08381f0cc1e09
Yuliia Pavlishak <juliapjo3@gmail.com>
2026-03-14 18:17:02 +0200

    override and update CreateCommand, HelpCommand

---
+++ GradeBook/UserInterfaces/StandardGradeBook.cs
@@ -0,0 +1,13 @@
+﻿using GradeBook.Enums;
+using GradeBook.GradeBooks;
+
+namespace GradeBook.UserInterfaces
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
